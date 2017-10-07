f9658aba54ea323b4f6133497cc3f679942aa067
sgc88 <sehribancheng@yahoo.com>
2017-10-06 20:29:30 -0700

    organized js filed

--- main.js
+++ main.js
@@ -1,84 +1,49 @@
-var faces = [];
-// var Tile=[];
+
 $(document).ready(function(){
 
   console.log("sanity check");
-
-//   // var card1=[]
-//
-//   $(".ln").on("click",function(){
-//     alert("clicked");
-// //     var boxNumber=this.getAttribute("img");
-// //     console.log("clicked");
-// // //visually flip the card
-// //if now only face up remember it and leave
+//basic steps of how game works
+//1-visually flip the card
+//2 if now only 1 face up remember it and leave
 // //otherwise
 //     //if its 2 cards compare them
 //         //if they are equal remove them
 //           //if no card left console.log 'win'
 //         //else flip back both of them
-// //
-// // var  playedCrads=[]
-// // var flipCard= function(){
-// //       if (cardsInPlay[0] ==== cardsInPlay[1]){
-// //         //
-// //         //remove the cards
-// //       }else{
-// //         //put them back
-// //       }
-// //
-// //
 
-// $("#box1").attr("name", "1");
-// $("#box2").attr("name", "2");
-// $("#box3").attr("name", "3");
-// $("#box4").attr("name", "4");
-// $("#box5").attr("name", "5");
-// $("#box6").attr("name", "6");
-// $("#box7").attr("name", "7");
-// $("#box8").attr("name", "8");
-// $("#box9").attr("name", "9");
-// $("#box10").attr("name", "10");
-// $("#box11").attr("name", "11");
-// $("#box12").attr("name", "12");
-// $("#box13").attr("name", "13");
-// $("#box14").attr("name", "14");
-// $("#box15").attr("name", "15");
-// $("#box16").attr("name", "16");
+// $("#box1").attr(
 var images = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 images = Shuffle(images);
 
-
+// set up shuffle methot for images
 var shuffled=function(){
   for(var i=1; i<=8; i++){
     $(".ln[name=$(i+1)]").attr('data-card', images[i])
   }
 }
-
+//loop through images into boxes
 for(var i=1; i<=16; i++){
   $(`#box${i}`).attr("data-card", images[i]);
 }
-
+// we should be able to shuffle the images so 2 images don't display next to eachother
 function Shuffle(o) {
 for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
 return o;
 };
 
-
-// for(var i=1; i<=8; i++){
-//   $(`.ln[name="${i}"]`).css('background-image', `url('pic${i}.jpg')`);
-//
-// }
-// $(`.ln[name="1"]`).css('background-image', "url('pic1.jpg')");
-// set click event to make sure we have access to the boxes
+// set click event to make sure we have access to the boxes and disyplay the images
 var clicks= $(".ln").on("click", function(e){
   var imageNumber = $(e.target).attr('data-card');
   $(this).css('background-image', `url('pic${imageNumber}.jpg')`);
 
-  // // var cards;
-  // var box=this.setAttribute(images.cardImage);
-  // $(this).css('background-image', ``);
+
+
 });
+
+
+// });
+
+
 
 
 
